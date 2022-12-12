5b3e09b634872f47b6fea131008899f34e720b7b
LingoNMBU <erlinge@nmbu.no>
2022-12-13 00:39:42 +0100

    file removal

---
+++ prob8.cpp
@@ -0,0 +1,87 @@
+#include "wavebox.hpp"
+using namespace std;
+
+int main()
+{
+    //sim params
+    double h   = 0.005; //Stepsize space
+    double dt  = 2.5e-5; //Stepsize time
+    double T   = 0.002; //Max time
+
+    //Packet params
+    double c_x = 0.25; //packet start center x
+    double s_x = 0.05; //packet start std x
+    double p_x = 200.; //packet start momentum x
+    double c_y = 0.5; //packet start center y
+    double s_y = 0.2; //packet start std y
+    double p_y = 0.0; //packet start momentum y
+
+    //potential params
+    double v_0 = 1.e10; //potential
+    double x_thick = 0.02;
+    double x_center = 0.5;
+    double slit_width1 = 0.05;
+    double wall_width1 = 0.05;
+    int n_slits1 = 1;
+    int n_slits2 = 2;
+    int n_slits3 = 3;
+
+    std::vector<double> slit_widths;
+    std::vector<double> wall_widths;
+    slit_widths.push_back(slit_width1);
+    slit_widths.push_back(slit_width1);
+    slit_widths.push_back(slit_width1);
+    wall_widths.push_back(wall_width1);
+    wall_widths.push_back(wall_width1);
+
+    bool prob8_single = true;
+    if (prob8_single)
+    {
+        Wavebox catbox8a = Wavebox(h, dt, T);
+
+        catbox8a.generate_slit_potential(x_thick, x_center, n_slits1 ,slit_widths ,wall_widths ,v_0);
+
+        catbox8a.initialize_packet(c_x, c_y, p_x, p_y, s_x, s_y);
+
+        catbox8a.simulate(true, false);
+
+        arma::cx_cube u_cube = catbox8a.u_cube;
+
+        u_cube.save("prob8a_u_cube_single");
+    }
+
+    bool prob8_double = true;
+    if (prob8_double)
+    {
+        Wavebox catbox8a = Wavebox(h, dt, T);
+
+        catbox8a.generate_slit_potential(x_thick, x_center, n_slits2 ,slit_widths ,wall_widths ,v_0);
+
+        catbox8a.initialize_packet(c_x, c_y, p_x, p_y, s_x, s_y);
+
+        catbox8a.simulate(true, false);
+
+        arma::cx_cube u_cube = catbox8a.u_cube;
+
+        u_cube.save("prob8a_u_cube_double");
+    }
+
+    bool prob8_triple = true;
+    if (prob8_triple)
+    {
+        Wavebox catbox8a = Wavebox(h, dt, T);
+
+        catbox8a.generate_slit_potential(x_thick, x_center, n_slits3 ,slit_widths ,wall_widths ,v_0);
+
+        catbox8a.initialize_packet(c_x, c_y, p_x, p_y, s_x, s_y);
+
+        catbox8a.simulate(true, false);
+
+        arma::cx_cube u_cube = catbox8a.u_cube;
+
+        u_cube.save("prob8a_u_cube_triple");
+    }
+    
+
+
+}
